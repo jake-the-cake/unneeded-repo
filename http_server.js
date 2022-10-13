d8ec5d06434ef263a1e87f1ed926da6faed635f5
jake-the-cake <askjake331@gmail.com>
2022-10-13 19:58:05 -0400

    accounts endpoints

--- http_server.js
+++ http_server.js
@@ -24,6 +24,64 @@ db.defaults({ users: [] }).write();
 
 let port = process.env.PORT || 3000;
 
+// account endpoints
+app.get('/accounts', (req, res) => {
+    const data = db.value()
+    res.send( data )
+})
+
+app.post('accounts', (req, res) => {
+    // setup
+          const firstName = faker.name.firstName();
+          const lastName = faker.name.lastName();
+          const date = faker.date.past(
+            50,
+            new Date("Sat Sep 20 1992 21:35:02 GMT+0200 (CEST)")
+          );
+
+          // random data
+          const name = faker.name.findName(firstName, lastName);
+          const email = faker.internet.email(firstName, lastName);
+          const username = faker.internet.userName(firstName, lastName);
+          const password = faker.internet.password();
+          const phone = faker.phone.phoneNumber();
+          const streetaddress = faker.address.streetAddress();
+          const citystatezip =
+            faker.address.city() +
+            ", " +
+            faker.address.stateAbbr() +
+            " " +
+            faker.address.zipCode();
+          const latitude = faker.address.latitude();
+          const longitude = faker.address.longitude();
+          const avatar = faker.internet.avatar();
+          const dob =
+            date.getFullYear() +
+            "-" +
+            (date.getMonth() + 1) +
+            "-" +
+            date.getDate();
+
+          // user data object for post
+          const user = {
+            name,
+            dob,
+            email,
+            username,
+            password,
+            phone,
+            streetaddress,
+            citystatezip,
+            latitude,
+            longitude,
+            avatar,
+          };
+            db.get('users').push(user).write();
+    console.log(db.get('users').value());
+    res.send(db.get('users').value());
+
+})
+
 // return all users
 app.get('/data', function (req, res) {
     res.send(db.get('users').value());
