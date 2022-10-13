2c866822c17ac1965cd40f9b4a1faac11716b6dc
jake-the-cake <askjake331@gmail.com>
2022-10-13 17:50:51 -0400

    add port

--- http_server.js
+++ http_server.js
@@ -58,6 +58,6 @@ app.post('/test', function(req, res){
 
 // start server
 // -----------------------
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('Running on port 3000!')
 })
